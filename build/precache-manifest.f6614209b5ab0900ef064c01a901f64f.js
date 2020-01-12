c0917a60d627fbf85abbb23a4f101914c910e449
wale-bant <walebant1@gmail.com>
2020-01-12 03:15:32 +0000

    complete

---
+++ build/precache-manifest.f6614209b5ab0900ef064c01a901f64f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e4d854d9afe53e9074a6daeaa360f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2aed6b1b416473a43c26",
+    "url": "./static/css/main.a1fba444.chunk.css"
+  },
+  {
+    "revision": "c637ffff87a0dffb728c",
+    "url": "./static/js/2.499aa0d4.chunk.js"
+  },
+  {
+    "revision": "601b632705bb04cea71932d5fa4ba9be",
+    "url": "./static/js/2.499aa0d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2aed6b1b416473a43c26",
+    "url": "./static/js/main.5b1eea4b.chunk.js"
+  },
+  {
+    "revision": "df8b5cd2c94327cfb757",
+    "url": "./static/js/runtime-main.caadb1c2.js"
+  }
+]);
